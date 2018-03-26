d8d8b1b5ee75710cbfda161ef4087f211d539fa0
Asvin Balloo <asvin.balloo@10up.com>
2018-03-26 17:38:08 +0400

    Upgrade Gulp from v3 to v4.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,33 +1,17 @@
 import gulp from 'gulp';
 import requireDir from 'require-dir';
-import runSequence from 'run-sequence';
 import livereload from 'gulp-livereload';
 
 requireDir( './gulp-tasks' );
 
-gulp.task( 'js', () => {
-	runSequence(
-		'webpack',
-	);
-} );
+gulp.task( 'js', gulp.series( 'webpack' ) );
 
-gulp.task( 'css', () => {
-	runSequence(
-		'cssnext',
-		'cssnano',
-		'cssclean'
-	);
-} );
+gulp.task( 'css', gulp.series( 'cssnext', 'cssnano', 'cssclean' ) );
 
 gulp.task( 'watch', () => {
 	livereload.listen( { basePath: 'dist' } );
-	gulp.watch( ['./assets/css/**/*.css', '!./assets/css/src/**/*.css'], ['css'] );
-	gulp.watch( './assets/js/**/*.js', ['js'] );
+	gulp.watch( ['./assets/css/**/*.css', '!./assets/css/src/**/*.css'], gulp.series( 'css' ) );
+	gulp.watch( './assets/js/**/*.js', gulp.series( 'js' ) );
 } );
 
-gulp.task( 'default', () => {
-	runSequence(
-		'css',
-		'webpack'
-	);
-} );
+gulp.task( 'default', gulp.parallel( 'css', 'webpack' ) );
