d5f9861d9415d7596b50e9f73c5aacf4dddabdf6
Taylor Lovett <tlovett88@gmail.com>
2018-07-10 14:08:47 -0400

    Move scripts to composer; fix composer error; simplify phpcs rules

--- includes/core.php
+++ includes/core.php
@@ -53,7 +53,7 @@ function theme_setup() {
 	// This theme uses wp_nav_menu() in three locations.
 	register_nav_menus(
 		array(
-			'primary'        => esc_html__( 'Primary Menu', 'tenup' ),
+			'primary' => esc_html__( 'Primary Menu', 'tenup' ),
 		)
 	);
 }
