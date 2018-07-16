df1995f5cd365e0574bb1d8cea834f5be6bbb8bc
Dave Ross <dave.ross@10up.com>
2018-07-16 16:36:34 -0400

    Remove TENUP_SCAFFOLD_URL (see #40)

--- functions.php
+++ functions.php
@@ -6,11 +6,10 @@
  */
 
 // Useful global constants.
-define( 'TENUP_SCAFFOLD_VERSION', '0.1.0' );
-define( 'TENUP_SCAFFOLD_URL', get_stylesheet_directory_uri() );
-define( 'TENUP_SCAFFOLD_TEMPLATE_URL', get_template_directory_uri() );
-define( 'TENUP_SCAFFOLD_PATH', get_template_directory() . '/' );
-define( 'TENUP_SCAFFOLD_INC', TENUP_SCAFFOLD_PATH . 'includes/' );
+define('TENUP_SCAFFOLD_VERSION', '0.1.0');
+define('TENUP_SCAFFOLD_TEMPLATE_URL', get_template_directory_uri());
+define('TENUP_SCAFFOLD_PATH', get_template_directory() . '/');
+define('TENUP_SCAFFOLD_INC', TENUP_SCAFFOLD_PATH . 'includes/');
 
 require_once TENUP_SCAFFOLD_INC . 'core.php';
 require_once TENUP_SCAFFOLD_INC . 'template-tags.php';
@@ -19,6 +18,6 @@ require_once TENUP_SCAFFOLD_INC . 'template-tags.php';
 TenUpScaffold\Core\setup();
 
 // Require Composer autoloader if it exists.
-if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-	require_once 'vendor/autoload.php';
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once 'vendor/autoload.php';
 }
