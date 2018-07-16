09db2cc2420fe2d2b6df59e4f5a6c5907f3ebd4a
Dave Ross <dave.ross@10up.com>
2018-07-16 16:39:40 -0400

    Code formatting

--- functions.php
+++ functions.php
@@ -6,10 +6,10 @@
  */
 
 // Useful global constants.
-define('TENUP_SCAFFOLD_VERSION', '0.1.0');
-define('TENUP_SCAFFOLD_TEMPLATE_URL', get_template_directory_uri());
-define('TENUP_SCAFFOLD_PATH', get_template_directory() . '/');
-define('TENUP_SCAFFOLD_INC', TENUP_SCAFFOLD_PATH . 'includes/');
+define( 'TENUP_SCAFFOLD_VERSION', '0.1.0' );
+define( 'TENUP_SCAFFOLD_TEMPLATE_URL', get_template_directory_uri() );
+define( 'TENUP_SCAFFOLD_PATH', get_template_directory() . '/' );
+define( 'TENUP_SCAFFOLD_INC', TENUP_SCAFFOLD_PATH . 'includes/' );
 
 require_once TENUP_SCAFFOLD_INC . 'core.php';
 require_once TENUP_SCAFFOLD_INC . 'template-tags.php';
