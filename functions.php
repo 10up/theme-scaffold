743a41ac559096f696b4ab82e8a7cc8c834a3d5f
Dave Ross <dave.ross@10up.com>
2018-07-16 16:40:47 -0400

    Code formatting

--- functions.php
+++ functions.php
@@ -18,6 +18,6 @@ require_once TENUP_SCAFFOLD_INC . 'template-tags.php';
 TenUpScaffold\Core\setup();
 
 // Require Composer autoloader if it exists.
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require_once 'vendor/autoload.php';
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once 'vendor/autoload.php';
 }
