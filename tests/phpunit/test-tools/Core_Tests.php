1f3708c9a1d12cc896686112e47fb3454415c19b
Dave Ross <dave.ross@10up.com>
2018-08-13 13:30:46 -0400

    Got unit tests working

--- tests/phpunit/test-tools/Core_Tests.php
+++ tests/phpunit/test-tools/Core_Tests.php
@@ -18,7 +18,7 @@ use TenUpScaffold as Base;
 class Core_Tests extends Base\TestCase {
 
 	protected $testFiles = [
-		'functions/core.php'
+		'core.php'
 	];
 
 	/**
@@ -26,9 +26,8 @@ class Core_Tests extends Base\TestCase {
 	 */
 	public function test_setup() {
 		// Setup
-		\WP_Mock::expectActionAdded( 'init', 'TenUpScaffold\Core\i18n' );
-		\WP_Mock::expectActionAdded( 'init', 'TenUpScaffold\Core\init' );
-		\WP_Mock::expectAction( 'tenup_scaffold_loaded' );
+		\WP_Mock::expectActionAdded( 'after_setup_theme', 'TenUpScaffold\Core\i18n' );
+		\WP_Mock::expectActionAdded( 'after_setup_theme', 'TenUpScaffold\Core\theme_setup' );
 
 		// Act
 		setup();
@@ -42,24 +41,10 @@ class Core_Tests extends Base\TestCase {
 	 */
 	public function test_i18n() {
 		// Setup
-		\WP_Mock::userFunction( 'get_locale', array(
+		\WP_Mock::onFilter( 'theme_locale' )->with( 'en_US', 'tenup-scaffold' )->reply( 'en_US' );
+		\WP_Mock::userFunction( 'load_theme_textdomain', array(
 			'times' => 1,
-			'args' => array(),
-			'return' => 'en_US',
-		) );
-		\WP_Mock::onFilter( 'plugin_locale' )->with( 'en_US', 'tenup-scaffold' )->reply( 'en_US' );
-		\WP_Mock::userFunction( 'load_textdomain', array(
-			'times' => 1,
-			'args' => array( 'tenup-scaffold', 'lang_dir/tenup-scaffold/tenup-scaffold-en_US.mo' ),
-		) );
-		\WP_Mock::userFunction( 'plugin_basename', array(
-			'times' => 1,
-			'args' => array( 'path' ),
-			'return' => 'path',
-		) );
-		\WP_Mock::userFunction( 'load_plugin_textdomain', array(
-			'times' => 1,
-			'args' => array( 'tenup-scaffold', false, 'path/languages/' ),
+			'args' => array( 'tenup-scaffold', 'path/languages' ),
 		) );
 
 		// Act
@@ -67,48 +52,5 @@ class Core_Tests extends Base\TestCase {
 
 		// Verify
 		$this->assertConditionsMet();
-	}
-
-	/**
-	 * Test initialization method.
-	 */
-	public function test_init() {
-		// Setup
-		\WP_Mock::expectAction( 'tenup_scaffold_init' );
-
-		// Act
-		init();
-
-		// Verify
-		$this->assertConditionsMet();
-	}
-
-	/**
-	 * Test activation routine.
-	 */
-	public function test_activate() {
-		// Setup
-		\WP_Mock::userFunction( 'flush_rewrite_rules', array(
-			'times' => 1
-		) );
-
-		// Act
-		activate();
-
-		// Verify
-		$this->assertConditionsMet();
-	}
-
-	/**
-	 * Test deactivation routine.
-	 */
-	public function test_deactivate() {
-		// Setup
-
-		// Act
-		deactivate();
-
-		// Verify
-		$this->assertTrue( true ); // Replace with actual assertion
 	}
 }
