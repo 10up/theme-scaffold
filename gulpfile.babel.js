a34563c1fa2259e6f2ec91e80f9a11dcf3b3c342
jodiwarren <jodi@jodiwarren.com>
2018-09-05 17:33:42 +0100

    Replace LiveReload with Browsersync Prevent reloading on admin, but allow admin-ajax to work.
    Closes #60

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,20 @@
 import gulp from 'gulp';
 import requireDir from 'require-dir';
-import livereload from 'gulp-livereload';
+import browserSync from 'browser-sync';
 
 requireDir( './gulp-tasks' );
+
+const bs = browserSync.create();
+
+gulp.task( 'bs-reload-css', ( cb ) => {
+	bs.reload('*.css');
+	cb();
+});
+
+gulp.task( 'bs-reload', ( cb ) => {
+	bs.reload();
+	cb();
+});
 
 gulp.task( 'js', gulp.series( 'webpack' ) );
 
@@ -10,9 +22,15 @@ gulp.task( 'cssprocess', gulp.series( 'css', 'cssnano', 'cssclean' ) );
 
 gulp.task( 'watch', () => {
 	process.env.NODE_ENV = 'development';
-	livereload.listen( { basePath: 'dist' } );
-	gulp.watch( ['./assets/css/**/*.css', '!./assets/css/src/**/*.css'], gulp.series( 'cssprocess' ) );
-	gulp.watch( './assets/js/**/*.js', gulp.series( 'js' ) );
+	bs.init({
+		proxy: 'local-theme-url.test',
+		snippetOptions: {
+			whitelist: ["/wp-admin/admin-ajax.php"],
+			blacklist: ["/wp-admin/**"]
+		}
+	});
+	gulp.watch( ['./assets/css/**/*.css', '!./assets/css/src/**/*.css'], gulp.series( 'cssprocess', 'bs-reload-css' ) );
+	gulp.watch( './assets/js/**/*.js', gulp.series( 'js', 'bs-reload' ) );
 } );
 
 gulp.task( 'default', gulp.parallel( 'cssprocess', gulp.series( 'set-prod-node-env', 'webpack' ) ) );
