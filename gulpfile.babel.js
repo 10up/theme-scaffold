02e6fcfeca8d1722fdcb2efc4c852d8acfb438a9
Damon Cook <damonacook@gmail.com>
2018-09-07 12:00:08 -0400

    replace BrowserSync with LiveReload in gulp watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -24,9 +24,17 @@ gulp.task( 'cssprocess', gulp.series( 'css', 'cssnano', 'cssclean' ) );
 
 gulp.task( 'watch', () => {
 	process.env.NODE_ENV = 'development';
-	livereload.listen( { basePath: 'dist' } );
-	gulp.watch( ['./assets/css/**/*.css', '!./assets/css/src/**/*.css'], gulp.series( 'cssprocess' ) );
-	gulp.watch( './assets/js/**/*.js', gulp.series( 'js' ) );
+	// https://browsersync.io/docs/options
+	bs.init({
+		open: false,     // Automatically open project in new tab?
+		proxy: localURL, // Set in package.json
+		snippetOptions: {
+			whitelist: ["/wp-admin/admin-ajax.php"],
+			blacklist: ["/wp-admin/**"]
+		}
+	});
+	gulp.watch( ['./assets/css/**/*.css', '!./assets/css/src/**/*.css'], gulp.series( 'cssprocess', 'bs-reload-css' ) );
+	gulp.watch( './assets/js/**/*.js', gulp.series( 'js', 'bs-reload' ) );
 } );
 
 gulp.task( 'default', gulp.parallel( 'cssprocess', gulp.series( 'set-prod-node-env', 'webpack' ) ) );
