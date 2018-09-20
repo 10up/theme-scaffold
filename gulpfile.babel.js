adfa89d8fd2e5fbe97d90cedff20a031b32b3dae
Damon Cook <damonacook@gmail.com>
2018-09-20 09:49:37 -0400

    add back simplified BS tasks and check
    Fancy != Better

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,9 +4,21 @@ import requireDir from 'require-dir';
 
 requireDir( './gulp-tasks' );
 
-var packageJson = require('../../../package.json');
+// We store some config objects here. So, let's load:
+const packageJson = require('../../../package.json');
+// Create a BrowserSync instance:
+const bs = browserSync.create();
+const proxyUrl = packageJson.browserSync.proxyUrl;
 
+gulp.task( 'bs-reload-css', ( cb ) => {
+	bs.reload('*.css');
+	cb();
+});
 
+gulp.task( 'bs-reload', ( cb ) => {
+	bs.reload();
+	cb();
+});
 
 gulp.task( 'js', gulp.series( 'webpack' ) );
 
@@ -15,8 +27,19 @@ gulp.task( 'cssprocess', gulp.series( 'css', 'cssnano', 'cssclean' ) );
 gulp.task( 'watch', () => {
 	process.env.NODE_ENV = 'development';
 
-	gulp.watch( ['./assets/css/**/*.css', '!./assets/css/src/**/*.css'], gulp.series( 'cssprocess', 'reload' ) );
-	gulp.watch( './assets/js/**/*.js', gulp.series( 'js', 'reload' ) );
+	if ( proxyUrl ) {
+		// https://browsersync.io/docs/options
+		bs.init({
+			proxy: packproxyUrl,
+			snippetOptions: {
+				whitelist: ["/wp-admin/admin-ajax.php"],
+				blacklist: ["/wp-admin/**"]
+			}
+		});
+	}
+
+	gulp.watch( ['./assets/css/**/*.css', '!./assets/css/src/**/*.css'], gulp.series( 'cssprocess', 'bs-reload-css' ) );
+	gulp.watch( './assets/js/**/*.js', gulp.series( 'js', 'bs-reload' ) );
 } );
 
 gulp.task( 'default', gulp.parallel( 'cssprocess', gulp.series( 'set-prod-node-env', 'webpack' ) ) );
