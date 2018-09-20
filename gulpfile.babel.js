a9cdd01e3009bcf10a7cbc525705b7a451973d0e
Damon Cook <damonacook@gmail.com>
2018-09-20 09:48:25 -0400

    remove fancy conditional BS logic
    truly was BS :-p

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,50 +1,12 @@
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
 import requireDir from 'require-dir';
-import requireUncached from 'require-uncached';
 
 requireDir( './gulp-tasks' );
 
 var packageJson = require('../../../package.json');
 
-/**
- * Conditionally set up BrowserSync.
- * Only run BrowserSync if packageJson.browserSync.live = true.
- */
 
-// Create a BrowserSync instance:
-const server = browserSync.create();
-
-// Initialize the BrowserSync server conditionally:
-function serve( done ) {
-	if ( packageJson.browserSync.live ) {
-		server.init( {
-			proxy: packageJson.browserSync.proxyUrl,
-			liveReload: true,
-			open: false, // Automatically open project in new tab?
-			snippetOptions: {
-				whitelist: ['/wp-admin/admin-ajax.php'],
-				blacklist: ['/wp-admin/**']
-			}
-		});
-	}
-	done();
-}
-
-// Reload the live site:
-function reload( done ) {
-	packageJson = requireUncached('../../../package.json');
-
-	if ( packageJson.browserSync.live ) {
-		if ( server.paused ) {
-			server.resume();
-		}
-		server.reload();
-	} else {
-		server.pause();
-	}
-	done();
-}
 
 gulp.task( 'js', gulp.series( 'webpack' ) );
 
