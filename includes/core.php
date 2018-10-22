cb7b0375fa9c40b50f36a087aea9b1239ae51c67
Chris Marslender <chrismarslender@gmail.com>
2018-10-22 11:47:14 -0700

    Run phpcs from the lint script. Add deploy-lint command to easily run linting commands on deploy

--- includes/core.php
+++ includes/core.php
@@ -44,7 +44,8 @@ function theme_setup() {
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support(
-		'html5', array(
+		'html5',
+		array(
 			'search-form',
 			'gallery',
 		)
