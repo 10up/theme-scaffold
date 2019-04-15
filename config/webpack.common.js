4fad4547035d68d4a20c26991c7f1a640efd0b2b
Jon Christensen <jon.christensen@10up.com>
2019-04-15 17:43:37 -0500

    update common to create base, es5, and es6

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -7,6 +7,7 @@ const FixStyleOnlyEntriesPlugin = require( 'webpack-fix-style-only-entries' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const StyleLintPlugin = require( 'stylelint-webpack-plugin' );
 const WebpackBar = require( 'webpackbar' );
+const merge = require( 'webpack-merge' );
 
 const isProduction = 'production' === process.env.NODE_ENV;
 
@@ -14,20 +15,80 @@ const isProduction = 'production' === process.env.NODE_ENV;
 const settings = require( './webpack.settings.js' );
 
 /**
- * Configure entries.
+ * Configure CSS entries.
  */
-const configureEntries = () => {
+const configureEntriesCss = () => {
 	const entries = {};
 
-	for ( const [ key, value ] of Object.entries( settings.entries ) ) {
+	for ( const [ key, value ] of Object.entries( settings.entries.css ) ) {
 		entries[ key ] = path.resolve( process.cwd(), value );
 	}
 
 	return entries;
 };
 
-module.exports = {
-	entry: configureEntries(),
+/**
+ * Configure JS entries.
+ */
+const configureEntriesJs = () => {
+	const entries = {};
+
+	for ( const [ key, value ] of Object.entries( settings.entries.js ) ) {
+		entries[ key ] = path.resolve( process.cwd(), value );
+	}
+
+	return entries;
+};
+
+const rules = {
+	lintjs: {
+		test: /\.js$/,
+		enforce: 'pre',
+		loader: 'eslint-loader',
+		options: {
+			fix: true
+		}
+	},
+	scripts: {
+		test: /\.js$/,
+		exclude: /node_modules/,
+		use: [
+			{
+				loader: 'babel-loader',
+				options: {
+					presets: [ '@babel/preset-env' ],
+					cacheDirectory: true,
+					sourceMap: ! isProduction,
+				},
+			},
+		],
+	},
+	styles: {
+		test: /\.css$/,
+		include: path.resolve( process.cwd(), settings.paths.src.css ),
+		use: [
+			{
+				loader: MiniCssExtractPlugin.loader,
+			},
+			{
+				loader: 'css-loader',
+				options: {
+					sourceMap: ! isProduction,
+					// We copy fonts etc. using CopyWebpackPlugin.
+					url: false,
+				},
+			},
+			{
+				loader: 'postcss-loader',
+				options: {
+					sourceMap: ! isProduction,
+				},
+			},
+		],
+	},
+};
+
+const defaults = {
 	output: {
 		path: path.resolve( process.cwd(), settings.paths.dist.base ),
 		filename: settings.filename.js,
@@ -51,55 +112,10 @@ module.exports = {
 	module: {
 		rules: [
 			// Lint JS.
-			{
-				test: /\.js$/,
-				enforce: 'pre',
-				loader: 'eslint-loader',
-				options: {
-					fix: true
-				}
-			},
+			rules.lintjs,
 
 			// Scripts.
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							presets: [ '@babel/preset-env' ],
-							cacheDirectory: true,
-							sourceMap: ! isProduction,
-						},
-					},
-				],
-			},
-
-			// Styles.
-			{
-				test: /\.css$/,
-				include: path.resolve( process.cwd(), settings.paths.src.css ),
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							sourceMap: ! isProduction,
-							// We copy fonts etc. using CopyWebpackPlugin.
-							url: false,
-						},
-					},
-					{
-						loader: 'postcss-loader',
-						options: {
-							sourceMap: ! isProduction,
-						},
-					},
-				],
-			},
+			rules.scripts
 		],
 	},
 
@@ -110,9 +126,6 @@ module.exports = {
 		new FixStyleOnlyEntriesPlugin( {
 			silent: true,
 		} ),
-
-		// Clean the `dist` folder on build.
-		new CleanWebpackPlugin(),
 
 		// Extract CSS into individual files.
 		new MiniCssExtractPlugin( {
@@ -139,3 +152,84 @@ module.exports = {
 		new WebpackBar(),
 	],
 };
+
+const es5 = merge.smart( defaults, {
+	name: 'es5',
+	entry: Object.assign( {},
+		configureEntriesJs()
+	),
+	output: {
+		filename: settings.filename.es5,
+	},
+	module: {
+		rules: [
+			// Scripts.
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [ [
+								'@babel/preset-env', {
+									targets: {
+										esmodules: false
+									}
+								}
+							] ],
+						},
+					},
+				],
+			},
+		]
+	}
+} );
+
+const es6 = merge.smart( defaults, {
+	name: 'es6',
+	entry: Object.assign( {},
+		configureEntriesJs(),
+		configureEntriesCss()
+	),
+	module: {
+		rules: [
+			// Scripts.
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [ [
+								'@babel/preset-env', {
+									targets: {
+										edge: '16',
+										firefox: '60',
+										chrome: '61',
+										safari: '11',
+										opera: '48',
+										ios: '11'
+									}
+								}
+							] ],
+						},
+					},
+				],
+			},
+
+			// Styles
+			rules.styles,
+		]
+	},
+	plugins: [
+		// Clean the `dist` folder on build.
+		new CleanWebpackPlugin(),
+	]
+} );
+
+module.exports = [
+	es5,
+	es6
+];
