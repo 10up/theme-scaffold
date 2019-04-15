e4e83b313087607e1dea30d40bd2fbabc0db7fc0
Damon Cook <damonacook@gmail.com>
2019-04-15 17:45:53 -0400

    add svg_icon() helper

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -61,3 +61,44 @@ function adjust_brightness( $hex, $steps ) {
 	return $return;
 
 }
+
+/**
+ * An svg template helper.
+ *
+ * @param  string  $name   The SVG Name.
+ * @param  array   $opts   Optional. The SVG options such as Role, Class, Label and Hidden.
+ * @param  boolean $output Either return or output the SVG Icon string.
+ * @return html
+ */
+function svg_icon( $name = '', $opts = array(), $output = false ) {
+	if ( empty( $name ) ) {
+		return '';
+	}
+
+	if ( is_customize_preview() ) {
+		$href = TENUP_SCAFFOLD_TEMPLATE_URL . '/dist/svg-sprite.svg#' . $name;
+	} else {
+		$href = '#' . $name;
+	}
+
+	$role = empty( $opts['role'] ) ? 'presentation' : $opts['role'];
+
+	// Fix 'image'.
+	if ( 'image' === $role ) {
+		$role = 'img';
+	}
+
+	$class = empty( $opts['class'] ) ? 'dmsb-icon dmsb-icon-' . $name : 'dmsb-icon ' . $opts['class'];
+	$label = empty( $opts['label'] ) ? $name : $opts['label'];
+	$hidden = empty( $opts['hidden'] ) ? '' : ' aria-hidden="' . esc_attr( $opts['hidden'] ) . '"';
+
+	$allowed_tags = get_post_with_svg_allowed_tags();
+
+	$svg_icon = sprintf( '<svg role="%s" class="%s" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-label="%s"%s><use xlink:href="%s"></use></svg>', esc_attr( $role ), esc_attr( $class ), esc_attr( $label ), $hidden, esc_url( $href ) );
+
+	if ( $output ) {
+		echo wp_kses( $svg_icon, $allowed_tags );
+		return;
+	}
+	return $svg_icon;
+}
