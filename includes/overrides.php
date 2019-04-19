f37041ad27775ddc3eb85a1cfc1d669fcae327a4
Christian Chung <christian.m.chung@gmail.com>
2019-04-19 15:14:33 -0400

    Cleanup docblocks and comments

--- includes/overrides.php
+++ includes/overrides.php
@@ -8,48 +8,54 @@
 namespace TenUpScaffold\Overrides;
 
 /**
- * Remove likely unused functionality that WordPress core enables
- * by default.
+ * Registers instances where we will override default WP Core behavior.
+ *
+ * @link https://developer.wordpress.org/reference/functions/print_emoji_detection_script/
+ * @link https://developer.wordpress.org/reference/functions/print_emoji_styles/
+ * @link https://developer.wordpress.org/reference/functions/wp_staticize_emoji/
+ * @link https://developer.wordpress.org/reference/functions/wp_staticize_emoji_for_email/
+ * @link https://developer.wordpress.org/reference/functions/wp_generator/
+ * @link https://developer.wordpress.org/reference/functions/wlwmanifest_link/
+ * @link https://developer.wordpress.org/reference/functions/rsd_link/
  *
  * @return void
  */
-function remove() {
+function setup() {
 	// Remove the Emoji detection script.
-	// https://developer.wordpress.org/reference/functions/print_emoji_detection_script/
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+
 	// Remove inline Emoji detection script.
-	// https://developer.wordpress.org/reference/functions/print_emoji_detection_script/
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+
 	// Remove Emoji-related styles from front end and back end.
-	// https://developer.wordpress.org/reference/functions/print_emoji_styles/
 	remove_action( 'wp_print_styles', 'print_emoji_styles' );
 	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
 	// Remove Emoji-to-static-img conversion.
-	// https://developer.wordpress.org/reference/functions/wp_staticize_emoji/
-	// https://developer.wordpress.org/reference/functions/wp_staticize_emoji_for_email/
 	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
 	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-	// Remove WordPress generator meta.
-	// https://developer.wordpress.org/reference/functions/wp_generator/
-	remove_action( 'wp_head', 'wp_generator' );
-	// Remove Windows Live Writer manifest link.
-	// https://developer.wordpress.org/reference/functions/wlwmanifest_link/
-	remove_action( 'wp_head', 'wlwmanifest_link' );
-	// Remove the link to Really Simple Discovery service endpoint.
-	// https://developer.wordpress.org/reference/functions/rsd_link/
-	remove_action( 'wp_head', 'rsd_link' );
 
 	add_filter( 'tiny_mce_plugins', __NAMESPACE__ . '\disable_emojis_tinymce' );
-	add_filter( 'wp_resource_hints', __NAMESPACE__ . '\disable_emojis_remove_dns_prefetch', 10, 2 );
+	add_filter( 'wp_resource_hints', __NAMESPACE__ . '\disable_emoji_dns_prefetch', 10, 2 );
+
+	// Remove WordPress generator meta.
+	remove_action( 'wp_head', 'wp_generator' );
+	// Remove Windows Live Writer manifest link.
+	remove_action( 'wp_head', 'wlwmanifest_link' );
+	// Remove the link to Really Simple Discovery service endpoint.
+	remove_action( 'wp_head', 'rsd_link' );
+
+
 }
 
 /**
  * Filter function used to remove the TinyMCE emoji plugin.
  *
  * @link https://developer.wordpress.org/reference/hooks/tiny_mce_plugins/
- * @param array $plugins An array of default TinyMCE plugins.
- * @return array An array of TinyMCE plugins, without wpemoji.
+ *
+ * @param  array $plugins An array of default TinyMCE plugins.
+ * @return array          An array of TinyMCE plugins, without wpemoji.
  */
 function disable_emojis_tinymce( $plugins ) {
 	if ( is_array( $plugins ) && in_array( 'wpemoji', $plugins ) ) {
@@ -61,11 +67,12 @@ function disable_emojis_tinymce( $plugins ) {
  * Remove emoji CDN hostname from DNS prefetching hints.
  *
  * @link https://developer.wordpress.org/reference/hooks/emoji_svg_url/
- * @param array  $urls URLs to print for resource hints.
- * @param string $relation_type The relation type the URLs are printed for.
- * @return array Difference betwen the two arrays.
+ *
+ * @param  array  $urls          URLs to print for resource hints.
+ * @param  string $relation_type The relation type the URLs are printed for.
+ * @return array                 Difference betwen the two arrays.
  */
-function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+function disable_emoji_dns_prefetch( $urls, $relation_type ) {
 	if ( 'dns-prefetch' === $relation_type ) {
 		/** This filter is documented in wp-includes/formatting.php */
 		$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
