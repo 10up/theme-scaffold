06e003a5adca845934987f5ebf92ba6479e8aa20
Christian Chung <christian.m.chung@gmail.com>
2019-04-19 15:23:52 -0400

    Update overrides require path and invocation

--- functions.php
+++ functions.php
@@ -12,11 +12,12 @@ define( 'TENUP_SCAFFOLD_PATH', get_template_directory() . '/' );
 define( 'TENUP_SCAFFOLD_INC', TENUP_SCAFFOLD_PATH . 'includes/' );
 
 require_once TENUP_SCAFFOLD_INC . 'core.php';
-require_once TENUP_SCAFFOLD_INC . 'header-cleanup.php';
+require_once TENUP_SCAFFOLD_INC . 'overrides.php';
 require_once TENUP_SCAFFOLD_INC . 'template-tags.php';
 
 // Run the setup functions.
 TenUpScaffold\Core\setup();
+TenUpScaffold\Overrides\setup();
 
 // Require Composer autoloader if it exists.
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
