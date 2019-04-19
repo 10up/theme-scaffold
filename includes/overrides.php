531b8a6eb15ebfda611e0314e2acb73ac629c33b
Christian Chung <christian.m.chung@gmail.com>
2019-04-19 15:18:21 -0400

    Reset array indices following array_diff

--- includes/overrides.php
+++ includes/overrides.php
@@ -77,7 +77,7 @@ function disable_emoji_dns_prefetch( $urls, $relation_type ) {
 		/** This filter is documented in wp-includes/formatting.php */
 		$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
 
-		$urls = array_diff( $urls, array( $emoji_svg_url ) );
+		$urls = array_values( array_diff( $urls, array( $emoji_svg_url ) ) );
 	}
 
 	return $urls;
