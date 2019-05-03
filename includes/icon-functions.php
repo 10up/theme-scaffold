94b8cad0de5317e7cd27b4e4d4182f65f75b0c1d
Damon Cook <damonacook@gmail.com>
2019-05-03 15:12:27 -0400

    refactor _svg_icon() function for more options

--- includes/icon-functions.php
+++ includes/icon-functions.php
@@ -6,55 +6,128 @@
  */
 
 /**
- * An svg template helper.
+ * An svg template helper with robust array of options.
  *
  * Example:
  * tenupscaffold_svg_icon(
- *     'instagram', array(
+ *     array(
+ *         'desc'   => 'Follow 10up on Instagram',
+ *         'fill'   => '#f4428f',
+ *         'height' => 24,
+ *         'icon'   => 'instagram',
+ *         'output' => true,
  *         'role'   => 'img',
- *         'class'  => 'tenup-icon instagram-icon',
- *         'label'  => 'Follow 10up on Instagram',
- *         'hidden' => 'true',
+ *         'title'  => 'Follow 10up on Instagram',
+ *         'width'  => 24,
  *     ),
- *     true,
  * );
  *
- * @param  string  $name   The SVG Name.
- * @param  array   $opts   Optional. The SVG options such as Role, Class, Label and Hidden.
- * @param  boolean $output Either return or output the SVG Icon string.
+ * @link   https://10up.github.io/Engineering-Best-Practices/markup/#svg-embedded-in-html
+ * @param  array $args   The SVG options such as Aria role, dimensions, title.
  * @return html
  */
-function tenupscaffold_svg_icon( $name = '', $opts = array(), $output = false ) {
-	if ( empty( $name ) ) {
-		return '';
+function tenupscaffold_svg_icon( $args = array() ) {
+	// Make sure $args are an array.
+	if ( empty( $args ) ) {
+		return esc_html__( 'Please define default parameters in the form of an array.', 'tenupscaffold' );
 	}
 
-	if ( is_customize_preview() ) {
-		$href = TENUP_SCAFFOLD_TEMPLATE_URL . '/dist/svg-sprite.svg#' . $name;
-	} else {
-		$href = '#' . $name;
+	// Define an icon.
+	if ( false === array_key_exists( 'icon', $args ) ) {
+		return esc_html__( 'Please define an SVG icon name.', 'tenupscaffold' );
 	}
 
-	$role = empty( $opts['role'] ) ? 'presentation' : $opts['role'];
+	// Set defaults.
+	$defaults = array(
+		'desc'   => '',
+		'fill'   => '',
+		'height' => '',
+		'icon'   => '',
+		'output' => false,
+		'role'   => 'presentation',
+		'title'  => '',
+		'width'  => '',
+	);
 
-	// Fix 'image'.
-	if ( 'image' === $role ) {
-		$role = 'img';
+	// Parse args.
+	$args = wp_parse_args( $args, $defaults );
+
+	// Figure out which title to use.
+	$block_title = ( $args['title'] ) ? $args['title'] : $args['icon'];
+
+	// Generate random IDs for the title and description.
+	$random_number  = wp_rand( 0, 99999 );
+	$block_title_id = 'title-' . sanitize_title( $block_title ) . '-' . $random_number;
+	$desc_id        = 'desc-' . sanitize_title( $block_title ) . '-' . $random_number;
+
+	// Set ARIA.
+	$aria_hidden     = ' aria-hidden="true"';
+	$aria_labelledby = '';
+	$aria_role       = ( $args['role'] ) ? ' role="' . $args['role'] . '"' : '';
+
+	if ( $args['title'] && $args['desc'] ) {
+		$aria_labelledby = ' aria-labelledby="' . $block_title_id . ' ' . $desc_id . '"';
+		$aria_hidden     = '';
 	}
 
-	$class = empty( $opts['class'] ) ? 'tenup-icon tenup-icon-' . $name : 'tenup-icon ' . $opts['class'];
-	$label = empty( $opts['label'] ) ? $name : $opts['label'];
-	$hidden = empty( $opts['hidden'] ) ? '' : ' aria-hidden="' . esc_attr( $opts['hidden'] ) . '"';
+	// Set SVG parameters.
+	$fill   = ( $args['fill'] ) ? ' fill="' . $args['fill'] . '"' : '';
+	$height = ( $args['height'] ) ? ' height="' . $args['height'] . '"' : '';
+	$width  = ( $args['width'] ) ? ' width="' . $args['width'] . '"' : '';
 
 	$allowed_tags = tenupscaffold_get_post_with_svg_allowed_tags();
 
-	$svg_icon = sprintf( '<svg role="%s" class="%s" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-label="%s"%s><use xlink:href="%s"></use></svg>', esc_attr( $role ), esc_attr( $class ), esc_attr( $label ), $hidden, esc_url( $href ) );
+	// Start a buffer...
+	ob_start();
+	?>
 
-	if ( $output ) {
-		echo wp_kses( $svg_icon, $allowed_tags );
-		return;
+	<svg
+	<?php
+		echo $fill; // WPCS XSS OK.
+		echo $height; // WPCS XSS OK.
+		echo $width; // WPCS XSS OK.
+	?>
+		class="icon icon-<?php echo esc_attr( $args['icon'] ); ?>"
+	<?php
+		echo $aria_hidden; // WPCS XSS OK.
+		echo $aria_labelledby; // WPCS XSS OK.
+		echo $aria_role; // WPCS XSS OK.
+	?>
+	>
+		<title id="<?php echo esc_attr( $block_title_id ); ?>">
+			<?php echo esc_html( $block_title ); ?>
+		</title>
+
+		<?php
+		// Display description if available.
+		if ( $args['desc'] ) :
+			?>
+				<desc id="<?php echo esc_attr( $desc_id ); ?>">
+					<?php echo esc_html( $args['desc'] ); ?>
+				</desc>
+		<?php endif; ?>
+
+		<?php
+		// Use absolute path in the Customizer so that icons show up in there.
+		if ( is_customize_preview() ) :
+			?>
+				<use xlink:href="<?php echo esc_url( get_parent_theme_file_uri( '/dist/svg/svg-sprite.svg#icon-' . esc_html( $args['icon'] ) ) ); ?>"></use>
+			<?php else : ?>
+				<use xlink:href="#icon-<?php echo esc_html( $args['icon'] ); ?>"></use>
+		<?php endif; ?>
+
+	</svg>
+
+	<?php
+	// Get the buffer.
+	$svg = ob_get_clean();
+
+	// Echo result.
+	if ( $args['output'] ) {
+		echo wp_kses( $svg, $allowed_tags );
 	}
-	return $svg_icon;
+
+	return $svg;
 }
 
 /**
@@ -65,17 +138,26 @@ function tenupscaffold_svg_icon( $name = '', $opts = array(), $output = false ) 
  */
 function tenupscaffold_get_post_with_svg_allowed_tags() {
 	$allowed_tags        = wp_kses_allowed_html( 'post' );
+	// SVG attributes we would like to allow.
 	$allowed_tags['svg'] = array(
-		'class'       => true,
-		'id'          => true,
-		'aria-hidden' => true,
-		'role'        => true,
-		'xmlns'       => true,
-		'xmlns:xlink' => true,
-		'aria-label'  => true,
+		'aria-hidden'     => true,
+		'aria-label'      => true,
+		'aria-labelledby' => true,
+		'fill'            => true,
+		'class'           => true,
+		'height'          => true,
+		'id'              => true,
+		'role'            => true,
+		'xmlns'           => true,
+		'xmlns:xlink'     => true,
+		'width'           => true,
 	);
+	// <use> attributes we would like to allow.
 	$allowed_tags['use'] = array(
 		'xlink:href' => true,
 	);
+	// <desc> we would like to allow.
+	$allowed_tags['desc'] = true;
+
 	return $allowed_tags;
 }
