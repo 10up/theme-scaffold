558c7f12a02c375e90f63033ccb4657863f4a5e8
Jared Rethman <jaredrethman@gmail.com>
2019-09-26 00:41:37 +0200

    Replaced babelrc with babel.config.js version.

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+/**
+ * Babel Config, .babelrc equivalent.
+ *
+ * @package TenUpThemeScaffold
+ *
+ * @type {{presets: [[]|String|Object]}}
+ */
+module.exports = {
+	'presets': [
+		[
+			/**
+			 * @link https://babeljs.io/docs/en/babel-preset-env#corejs
+			 */
+			'@babel/preset-env',
+			{
+				useBuiltIns: 'usage',
+				corejs: {
+					version: 3,
+					proposals: true
+				},
+			}
+		],
+		'@babel/preset-react',
+		'@wordpress/default'
+	],
+};
