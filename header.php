e4e50e86d8e948839f9a12c5987bfd25b2b1036e
Tim Wright <timwright12@gmail.com>
2020-06-08 13:34:26 -0400

    Update header.php

--- header.php
+++ header.php
@@ -11,7 +11,6 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta name="theme-color" content="#d23226" />
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
